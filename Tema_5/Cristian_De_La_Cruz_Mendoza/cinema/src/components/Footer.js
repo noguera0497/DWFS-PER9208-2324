d598aa34e9799b05e143671df3546b4d79b23371
Cristian De La Cruz <163951971+cristiandelacruz290@users.noreply.github.com>
2024-04-28 10:54:16 -0500

    Actividad Tema 5 (#120)
    Co-authored-by: cristiandelacruz90 <cristian.delacruz@ipsp-profremar.com>
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_5/Cristian_De_La_Cruz_Mendoza/cinema/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div className="pantalla3">
+            Cine UNIR-CINEMA
+            <br/>
+            Dirección: Calle Avenida de la Paz, 137. Logroño
+            <br/>
+            Teléfono: 941209743
+            <br/>
+            Email: cinefullstack@unircinema.com
+        </div>
+    );
+}
